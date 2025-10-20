5d168610e6ba41b7a1923e1905d91c3231a7e51d
prain1231-art <prain1231@gmail.com>
2025-10-20 18:24:08 +0800

    4th commmit

--- src/HelloWorldMethod.java
+++ src/HelloWorldMethod.java
@@ -2,7 +2,7 @@ public class HelloWorldMethod {
 
     public static void printHelloWorld() {
         System.out.println("Kamusta Mundo!");
-
-
+        System.out.println("Kamusta Mundo!");
+        System.out.println("Kamusta Mundo!");
     }
 }
