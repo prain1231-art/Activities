7a4d7ddc0540c4a41ec05e70e82fda31f8e57ee2
prain1231-art <prain1231@gmail.com>
2025-10-20 18:15:17 +0800

    1st commmit

---
+++ src/addition.java
@@ -0,0 +1,10 @@
+public class addition {
+   public static int bulsaA = 5;
+   public static int bulsaB  = 10;
+
+    public static int pera (int total) {
+        total = bulsaA + bulsaB;
+        return total;
+    }
+
+}
